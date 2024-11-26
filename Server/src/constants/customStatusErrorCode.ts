15056c859a9f356fe896a2378091b21eb7c71a1a
Priyanjith N <jithpriyan2006@gmail.com>
2024-11-26 16:50:50 +0530

    added custom error class for handeling validation

---
+++ Server/src/constants/customStatusErrorCode.ts
@@ -0,0 +1,12 @@
+export const ErrorCode = Object.freeze({
+    CREDENTIALS_NOT_GIVEN_OR_NOT_FOUND: "404x1 Required credentials not found or not given",
+    PROVIDE_VAILD_EMAIL: "400x1 Not a vaild email provide a vaild email",
+    PASSWORD_INVAILD: "400x2 both password and confirm password didn't match",
+    EMAIL_TAKEN: "400x3 email is in use",
+    USERNAME_TAKEN: "400x4 username is in use",
+    USER_NOT_FOUND: "404x2 user with that email not found",
+    PASSWORD_INCORRECT: "400x5 password incorrect",
+    TOKEN_NOT_FOUND: "404x3 token not found not AUTHENTICATED",
+    TOKEN_PAYLOAD_NOT_VALID: "400x6 token payload not vaild",
+    TOKEN_EXPIRED_NEW_TOKEN_NEEDED: "401x1 token expired new token needed"
+});
--- Server/src/usecase/auth.usecase.ts
+++ Server/src/usecase/auth.usecase.ts
@@ -1,10 +1,19 @@
+// constants
+import { ErrorCode } from "../constants/customStatusErrorCode";
+import { ErrorMessage } from "../constants/errorMesaage";
+
+// custom error classes
+import RequiredCredentialsNotGiven from "../errors/requiredCredentialsNotGiven.error";
+
 // interfaces
-import { IRegisterationCredentials } from "../entity/IUser.entity";
 import IAuthUseCase from "../interface/usecase/IAuth.usecase.interface";
+import { IRegisterationCredentials } from "../entity/IUser.entity";
 
 export default class AuthUseCase implements IAuthUseCase {
     async handelUserRegister(data: IRegisterationCredentials): Promise<string | never> {
         try {
+            if(!data.userName || !data.email || !data.password || !data.confirmPassword) throw new RequiredCredentialsNotGiven(ErrorMessage.REQUIRED_CREDENTIALS_NOT_GIVEN, ErrorCode.CREDENTIALS_NOT_GIVEN_OR_NOT_FOUND);
+
             
             return "";
         } catch (err) {
